638f42215d42d21055bb3795969e35314ba14bfe
Emil Wasilewski <emil.wasilewski@itmagination.pl>
2016-04-18 11:43:52 +0200

    commit

--- Artifacts/Install_Lamp/linux_install_lamp_v1.sh
+++ Artifacts/Install_Lamp/linux_install_lamp_v1.sh
@@ -5,7 +5,7 @@ apt-get -y update
 apt-get -y install apache2 php5 
 
 # write some PHP
-echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
+echo \<center\>\<h1\>3CityNetConf Demo\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
 # restart Apache
